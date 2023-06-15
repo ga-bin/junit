17356b7753fa336418ea196563ce9a288d433282
gbkim <gb.kim@fusionsoft.co.kr>
2023-06-15 15:04:49 +0900

    junit study commit

---
+++ lotto/src/test/java/com/junit/lotto/LottoNumberGeneratorTest.java
@@ -0,0 +1,55 @@
+package com.junit.lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoNumberGeneratorTest {
+
+	@DisplayName("로또 번호 갯수 테스트")
+	@Test
+	void lottoNumberSizeTest() {
+		// given
+		final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+		final int price = 1000;
+		
+		// when
+		final List<Integer> lottoNumber = lottoNumberGenerator.generate(price);
+		
+		// then
+		assertThat(lottoNumber.size()).isEqualTo(6);
+	}
+	
+	@DisplayName("로또 범위 테스트")
+	@Test
+	void lottoNumberRangeTest() {
+		// given
+		final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+		final int price = 1000;
+		
+		// when
+		final List<Integer> lottoNumber = lottoNumberGenerator.generate(price);
+		
+		// then
+		assertThat(lottoNumber.stream().allMatch(v -> v >= 1 && v <= 45)).isTrue();
+	}
+	
+	
+	@DisplayName("잘못된 로또 금액 테스트")
+	@Test
+	void lottoNumberInvalidMoneyTest() {
+		// given
+		final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+		final int price = 2000;
+		
+		// when
+		final RuntimeException exception = assertThrows(RuntimeException.class, () -> lottoNumberGenerator.generate(price));
+		
+		// then
+		assertThat(exception.getMessage()).isEqualTo("올바른 금액이 아닙니다");
+	}
+}
